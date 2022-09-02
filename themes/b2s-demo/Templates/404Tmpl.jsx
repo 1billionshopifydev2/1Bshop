1fb85fc28de804801d0c618ac48a011b77b78dad
Matt Mortek <matt@b2storefront.com>
2022-09-02 13:31:45 +0200

    feat(B2S-19): Add 404 page

--- themes/b2s-demo/Templates/404Tmpl.jsx
+++ themes/b2s-demo/Templates/404Tmpl.jsx
@@ -1,60 +1,19 @@
+import SEO from '@b2storefront/b2s_core/dist/components/snippets/SEO'
 import React from 'react'
+import { Layout } from '../../../src/components/Layout/Layout'
 
-import * as Components from '../../../src/pages/404'
-
-const Page404Tmpl = props => (
-  <div className="page-not-found">
-    <Components.SEO title="404: Not found" />
-    <div className="page-not-found-card">
-      <div className="page-not-found-card-header">
-        <h3>Not Found</h3>
-        <span>404</span>
+const Page404Tmpl = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <section className="section section-404">
+      <div className="container pt-5 pb-5 mt-5 mb-5">
+        <h3 className="text-center mt-5 pt-5">Page Not Found</h3>
+        <p className="text-center mb-5 pb-5">
+          The page you are looking for is not found.
+        </p>
       </div>
-      <form
-        action="/"
-        method="post"
-        onSubmit={props.handleSearchForm}
-        className="page-not-found-cart-body"
-      >
-        <div>
-          <p className="mb-2 text-center">
-            The page you were looking for does not exist.
-          </p>
-          <p className="mb-2 text-center">
-            Maybe you want to try the search instead?
-          </p>
-        </div>
-        <div className="pt-2">
-          <div className="page-not-found-form">
-            <input
-              defaultValue={props.defaultSearchParam}
-              name="q"
-              type="text"
-              autoComplete="off"
-            />
-            <button type="submit">Search</button>
-          </div>
-        </div>
-      </form>
-      <div className="d-flex px-2 py-2 justify-content-center">
-        Back to{' '}
-        <Components.Link className="mx-1" to="/">
-          A+R
-        </Components.Link>
-        <>
-          <div>|</div>
-          <button
-            className="ms-1 p-0 btn btn-link"
-            onClick={() => {
-              props.handlePreviousPage()
-            }}
-          >
-            Previous Page
-          </button>
-        </>
-      </div>
-    </div>
-  </div>
+    </section>
+  </Layout>
 )
 
 export default Page404Tmpl
